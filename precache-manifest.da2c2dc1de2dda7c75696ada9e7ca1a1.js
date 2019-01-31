e999b594962b11f1b45db5699e7605b52d2d21e0
vuanhatvip <volehoangnhat7296@gmail.com>
2019-01-31 23:50:35 +0700

    Updates

---
+++ precache-manifest.da2c2dc1de2dda7c75696ada9e7ca1a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7010e9591d3b26f37b76",
+    "url": "/ReactJSWebDemoSaga/static/js/runtime~main.7010e959.js"
+  },
+  {
+    "revision": "3431b5d7036b1f3642f6",
+    "url": "/ReactJSWebDemoSaga/static/js/main.3431b5d7.chunk.js"
+  },
+  {
+    "revision": "6044df34620abb5d2fca",
+    "url": "/ReactJSWebDemoSaga/static/js/1.6044df34.chunk.js"
+  },
+  {
+    "revision": "3431b5d7036b1f3642f6",
+    "url": "/ReactJSWebDemoSaga/static/css/main.b178eb74.chunk.css"
+  },
+  {
+    "revision": "88745ecaba239866f3aace9e5c0d5dea",
+    "url": "/ReactJSWebDemoSaga/index.html"
+  }
+];
